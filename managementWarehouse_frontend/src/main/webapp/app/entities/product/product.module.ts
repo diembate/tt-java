9989339c1bec91d6ef734211f6d1f567ed0f6cb4
diem diem <diembate@gmail.com>
2019-12-30 09:32:27 +0700

    jhipster

--- managementWarehouse_frontend/src/main/webapp/app/entities/product/product.module.ts
+++ managementWarehouse_frontend/src/main/webapp/app/entities/product/product.module.ts
@@ -7,13 +7,13 @@ import { ProductDetailComponent } from './product-detail.component';
 import { ProductUpdateComponent } from './product-update.component';
 import { ProductDeleteDialogComponent } from './product-delete-dialog.component';
 import { productRoute } from './product.route';
-import { ImportDetailInfoUpdateComponent } from './import/import-detail-info-update/import-detail-info-update.component';
 import { OrderDetailInfoUpdateComponent } from './order/order-detail-info-update/order-detail-info-update.component';
+import { ImportDetailInfoUpdateComponent } from './import/import-detail-info-update/import-detail-info-update.component';
 
 
 @NgModule({
   imports: [ManagementWarehouseSharedModule, RouterModule.forChild(productRoute)],
-  declarations: [ProductComponent, ProductDetailComponent, ProductUpdateComponent, ProductDeleteDialogComponent, ImportDetailInfoUpdateComponent, OrderDetailInfoUpdateComponent],
-  entryComponents: [ProductDeleteDialogComponent,ImportDetailInfoUpdateComponent,OrderDetailInfoUpdateComponent]
+  declarations: [ProductComponent, ProductDetailComponent, ProductUpdateComponent, ProductDeleteDialogComponent, OrderDetailInfoUpdateComponent,ImportDetailInfoUpdateComponent],
+  entryComponents: [ProductDeleteDialogComponent,OrderDetailInfoUpdateComponent,ImportDetailInfoUpdateComponent]
 })
 export class ManagementWarehouseProductModule {}
